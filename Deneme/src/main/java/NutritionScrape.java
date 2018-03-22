b24ffa89dfb42a7837b34a3c0e2b7924dedbde56
MustafaFidan <fidanmustafa95@gmail.com>
2018-03-22 15:32:33 +0300

    Update NutritionScrape.java

--- Deneme/src/main/java/NutritionScrape.java
+++ Deneme/src/main/java/NutritionScrape.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.interactions.Actions;
 public class NutritionScrape {
 
 	public static void main(String[] args) throws InterruptedException {
-//				System.setProperty("webdriver.chrome.driver", "F:\\UNIVERSITY\\CS-491_492\\Code\\chromedriver.exe");
 //				WebDriver driver = new ChromeDriver();
 		HtmlUnitDriver driver = new HtmlUnitDriver();
 //				driver.setJavascriptEnabled(true);	
